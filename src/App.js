7eee55f820658cf3b7fdc95c8837e4039a7c31c6
sourav-mahmud <souravmahmud32@gmail.com>
2024-01-22 00:46:53 +0600

    tree created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const TreeNode = ({ node, position }) => {
+  return (
+    <div className={`node ${position}`}>
+      <div className="box">
+        <div className="title">{node.title}</div>
+      </div>
+      {node.child && (
+        <div className="child-container">
+          {node.child.map((childNode, index) => (
+            <TreeNode key={index} node={childNode} position={position} />
+          ))}
+        </div>
+      )}
+      {node.curve && <div className="curve"></div>}
+    </div>
+  );
+};
+
+const Tree = ({ data }) => {
+  return (
+    <div className="tree-container">
+      {data.map((node, index) => (
+        <TreeNode key={index} node={node} position="center" />
+      ))}
+    </div>
+  );
+};
+
 function App() {
+  const data = [
+    {
+      "title": "Tree Start"
+    },
+    {
+      "title": "Step 1"
+    },
+    {
+      "title": "Step 2",
+      "child": [
+        {
+          "title": "Sub Tree",
+          "curve": true,
+          "child": [
+            {
+              "title": "Step 1"
+            }
+          ]
+        },
+        {
+          "title": "Sub Tree",
+          "curve": true,
+          "child": [
+            {
+              "title": "Step 1"
+            }
+          ]
+        }
+      ]
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tree data={data} />
     </div>
   );
 }
